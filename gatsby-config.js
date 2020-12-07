e2066646322a26cb6773416281666aa6f0ef73c0
Jacy L Hoag <jacylh@gmail.com>
2020-12-07 15:55:28 -0500

    updated google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-E9W4B523QY", // Google Analytics / GA
+          "G-VXG711Q550", // Google Analytics / GA
         ],
       },
     },
